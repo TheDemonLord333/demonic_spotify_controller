c86d8094778ccd298b811460ac075bf6a40cad18
TheDemonLord333 <thedemonlord333@gmail.com>
2026-09-04 00:46:05 +0200

    Initial Commit

---
+++ DemonicSpotifyController/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DemonicSpotifyController
+//
+//  Created by David Martens on 04.09.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ DemonicSpotifyControllerTests/DemonicSpotifyControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemonicSpotifyControllerTests.swift
+//  DemonicSpotifyControllerTests
+//
+//  Created by David Martens on 04.09.26.
+//
+
+import Testing
+@testable import DemonicSpotifyController
+
+struct DemonicSpotifyControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
